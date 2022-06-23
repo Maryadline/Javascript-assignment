e01e4f8667e721202bd0325e7cf2c2172198b2f5
Maryadline <Cmaryadline@gmail.com>
2022-06-23 07:25:31 +0100

    JavaScript file

---
+++ data.js
@@ -0,0 +1,7 @@
+let name = 'Maryadline Chinonso Ikwuegbu'
+let height = '1.75m'
+let country = 'Nigeria'
+
+document.write('Name:' + ' ' + name + ' ' + '<br>')
+document.write('Height:' + ' ' + height + ' ' + '<br>')
+document.write('Country:' + ' ' + country + ' ' + '<br>')
